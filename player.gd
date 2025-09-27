fbc3004c235e02a6ab3f1bbfa817e2e915839817
aranock <aranockofthevoid@gmail.com>
2025-09-27 10:20:58 -0600

    Simplified movement, adjusted speed

--- player.gd
+++ player.gd
@@ -1,16 +1,11 @@
 extends CharacterBody2D
 
-var player_speed : float = 50.0
-var player_is_moving : bool = false
+var player_speed : float = 200.0
 
 func _physics_process(delta):
 	if Input.is_action_pressed("down") and not Input.is_action_pressed("up"):
 		velocity = Vector2(0,1) * player_speed
-		player_is_moving = true
+		move_and_slide()
 	elif Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
 		velocity = Vector2(0,-1) * player_speed
-		player_is_moving = true
-	else:
-		player_is_moving = false
-	if player_is_moving:
 		move_and_slide()

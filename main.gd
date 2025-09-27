55d1f337ac8928201fc940e9b086003ff48c8f48
GameJam Account003 <gamejam003@saskpolytech.ca>
2025-09-27 13:05:18 -0600

    Added movement clamping

--- main.gd
+++ main.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
 @onready var player = $Player
+@onready var camera = $Camera2D
+
 
 func _ready() -> void:
 	self.player.position = Vector2(576, 324)
+	self.player.player_movement.connect(_on_player_emit_player_movement)
+
+func _on_player_emit_player_movement() -> void:
+	print("moved")
+	var view = get_viewport_rect().size / 2
+	var camera_pos = camera.global_position
+	var bounds_uw = camera_pos.y - view.y #the camera bounds at the top
+	var bounds_dw = camera_pos.y + view.y #the camera bounds at the bottom
+	player.global_position.y = clamp(player.global_position.y, bounds_uw, bounds_dw)
--- player.gd
+++ player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 var player_speed : float = 200.0
+signal player_movement
 
 func _physics_process(delta):
 	if Input.is_action_pressed("down") and not Input.is_action_pressed("up"):
 		velocity = Vector2(0,1) * player_speed
 		move_and_slide()
+		emit_signal("player_movement")
 	elif Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
 		velocity = Vector2(0,-1) * player_speed
 		move_and_slide()
+		emit_signal("player_movement")

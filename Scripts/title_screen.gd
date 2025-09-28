ea8cacd9d6be06e9c33d772e0a758615e67fddb3
Penelope Tweten <penelopetweten@gmail.com>
2025-09-28 13:46:34 -0600

    fuck git

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+@onready var controls_image = $ControlsImage
+
 func _on_play_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
 func _on_controls_button_down() -> void:
-	pass # Replace with function body.
+	if controls_image.visible == true:
+		controls_image.visible = false
+	else:
+		controls_image.visible = true
 
 
 func _on_controls_2_button_down() -> void:
